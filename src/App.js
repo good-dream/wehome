83f0536e7903acf8cf5947db9ecbe7a89b8c6c56
Your Name <you@example.com>
2019-09-02 21:41:55 +0800

    项目搭建基本完成

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// 该文件为根组件
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 引入react
+import React, { Component, Fragment } from 'react'
+// 引入路由及相关
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+// 引入相应的组件
+import HKLayout from './components/HKLayout';
+
+
+// 定义一个类组件
+class App extends Component {
+    // 挂载时的生命周期函数
+    componentDidMount(){
+
+    }
+
+    render (){
+        return (
+            <Fragment>
+                
+            </Fragment>
+        )
+    }
 }
 
+
+// 把根组件暴露出去
 export default App;
